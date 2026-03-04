f7837c1c8d6704fb7e6eaafdee70bd131973dacd
Anfsity <186920685+anfsity@users.noreply.github.com>
2026-03-04 16:11:01 +0800

    feat: add cpo.cpp

---
+++ execution/cpo.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <type_traits>
+
+template <typename T> void my_algo(T &a, T &b) {
+  // 错误写法 1：强制使用标准库
+  std::swap(a, b);
+  // 后果：如果 T 有自定义的高效 swap（比如只交换指针），这里却用了
+  // std::swap（也就是三次拷贝），性能极差。
+
+  // 错误写法 2：直接调用
+  swap(a, b);
+  // 后果：如果 T 是标准类型（如 int），这里编译不过，因为找不到 swap。
+}
+
+template <typename T> void my_algo_(T &a, T &b) {
+  using std::swap; // 1. 引入标准库兜底
+  swap(a,
+       b); // 2. 让 ADL (参数依赖查找) 决定是用 std::swap 还是 T 自定义的 swap
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+namespace standard {
+namespace detail {
+void tag_invoke();
+struct tag_invoke_t {
+  template <typename Tag, typename... Args>
+  constexpr auto operator()(Tag tag, Args &&...args) const
+      noexcept(noexcept(tag_invoke(static_cast<Tag &&>(tag),
+                                   static_cast<Args &&>(args)...)))
+          -> decltype(tag_invoke(static_cast<Tag &&>(tag),
+                                 static_cast<Args &&>(args)...)) {
+    return tag_invoke(static_cast<Tag &&>(tag), static_cast<Args &&>(args)...);
+  }
+};
+} // namespace detail
+
+inline constexpr detail::tag_invoke_t tag_invoke{}; // cpo
+
+template <auto &Tag> using tag_t = std::decay_t<decltype(Tag)>;
+} // namespace standard
+
+namespace standard {
+
+namespace detail {
+struct do_something_t {
+  template <typename T> void operator()(T &t) noexcept {
+    tag_invoke(do_something_t{}, t); // function
+  }
+};
+
+// 注意函数定义不再是do_something，而是tag_invoke，tag就是
+// detail::do_something_t
+template <typename T> void tag_invoke(do_something_t, T &) noexcept {
+  std::cout << "standard do something" << std::endl;
+}
+} // namespace detail
+
+inline detail::do_something_t do_something{};
+} // namespace standard
+
+namespace thirdparty {
+
+struct complicated_structure {
+  //....各种指针，引用
+
+  // tag_t<do_something>就是standard::detail::do_something_t
+  friend void tag_invoke(standard::tag_t<standard::do_something>,
+                         complicated_structure &) noexcept {
+    std::cout << "customized do something" << std::endl;
+  }
+};
+
+struct simple_structure {};
+} // namespace thirdparty
+
+int main() {
+  thirdparty::simple_structure s;
+  standard::do_something(s);
+
+  thirdparty::complicated_structure c;
+  standard::do_something(c);
+
+  using namespace standard;
+  do_something(s);
+  do_something(c);
+
+  return 0;
+}
