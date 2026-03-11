377e08d376b20fcd1b0aafaaa2ec340ac6c25e64
Anfsity <186920685+anfsity@users.noreply.github.com>
2026-03-11 11:32:25 +0800

    feat: update reflection.cpp

--- meta/reflection.cpp
+++ meta/reflection.cpp
@@ -1,13 +1,39 @@
+#include <cassert>
 #include <meta>
 #include <print>
-#include <cassert>
-#include <vector>
+#include <string>
 #include <type_traits>
+#include <vector>
 
 // 反射！！！
 // Reflection !!!
 
 constexpr auto no_check = std::meta::access_context::unchecked();
+
+template <std::meta::info R> std::string printFullName() {
+  if constexpr (R == std::meta::info()) {
+    return "";
+  }
+
+  constexpr auto parent = std::meta::parent_of(R);
+
+  std::string res;
+
+  if constexpr (parent != std::meta::info() && parent != ^^::) {
+    res += printFullName<parent>();
+    res += "::";
+  }
+
+  if constexpr (std::meta::has_identifier(R)) {
+    res += std::format("{}", std::meta::identifier_of(R));
+  } else {
+    res += std::format("{}", std::meta::display_string_of(R));
+  }
+
+  return res;
+}
+
+//////////////////////////////////////////////////////////////////
 
 struct Point {
   int x;
@@ -20,42 +46,68 @@ struct Math {
   int operator+(Math) { return 1; }
 };
 
-}
+} // namespace detail
 
-template <typename T>
-struct foo : std::true_type {};
+template <typename T> struct foo : std::true_type {};
 
 void test1() {
   Point p{1, 2};
-  constexpr auto rx = std::meta::nonstatic_data_members_of(^^Point, no_check)[0];
-  constexpr auto ry = std::meta::nonstatic_data_members_of(^^Point, no_check)[1];
+  constexpr auto rx =
+      std::meta::nonstatic_data_members_of(^^Point, no_check)[0];
+  constexpr auto ry =
+      std::meta::nonstatic_data_members_of(^^Point, no_check)[1];
 
   p.[:rx:] = 3;
   p.[:ry:] = 4;
 
   std::print("{}, {}\n", p.x, p.y);
 
-  std::print("Point: {}, {}\n", std::meta::identifier_of(rx), std::meta::identifier_of(ry)); // Point: x, y
+  std::print("Point: {}, {}\n", std::meta::identifier_of(rx),
+             std::meta::identifier_of(ry));             // Point: x, y
   std::print("{}\n", std::meta::display_string_of(rx)); // x
-  std::print("{}::{}\n", std::meta::display_string_of(std::meta::parent_of(rx)), std::meta::display_string_of(rx)); // Point::x
-  std::print("{}::{}\n", std::meta::identifier_of(std::meta::parent_of(rx)), std::meta::identifier_of(rx)); // Point::x
+  std::print("{}::{}\n", std::meta::display_string_of(std::meta::parent_of(rx)),
+             std::meta::display_string_of(rx)); // Point::x
+  std::print("{}::{}\n", std::meta::identifier_of(std::meta::parent_of(rx)),
+             std::meta::identifier_of(rx)); // Point::x
 
   std::vector<int> arr = {1, 2, 3, 4};
-  constexpr std::meta::info rarr = ^^std::vector;  
+  constexpr std::meta::info rarr = ^^std::vector;
   constexpr auto loc = std::meta::source_location_of(rarr);
-  std::print("file name: {}\nline: {}\ncolumn: {}\nfunction name: {}\n", loc.file_name(), loc.line(), loc.column(), loc.function_name());
+  std::print("file name: {}\nline: {}\ncolumn: {}\nfunction name: {}\n",
+             loc.file_name(), loc.line(), loc.column(), loc.function_name());
 
   constexpr auto op = ^^detail::Math::operator+;
   std::print("{}\n", std::meta::display_string_of(op));
-  // std::print("{}\n", std::meta::identifier_of(op)); 
-  // error: call to consteval function 'std::meta::identifier_of' is not a constant expression
+  // std::print("{}\n", std::meta::identifier_of(op));
+  // error: call to consteval function 'std::meta::identifier_of' is not a
+  // constant expression
 
   constexpr auto r = ^^foo<int>;
   std::print("{}\n", std::meta::display_string_of(r));
+  std::print("{}\n", printFullName<r>());
+  std::print("{}\n", printFullName<op>());
 }
 
-int main () {
-  constexpr std::meta::info rint = ^^int; // Ｏ(≧▽≦)Ｏ 
+//////////////////////////////////////////////////////////////////
+
+void test2() {
+
+  constexpr auto rarr = ^^std::vector<int>;
+
+  static_assert(^^int == ^^int);
+  static_assert(^^int != ^^const int);
+  static_assert(^^int != ^^const int &);
+
+  using alias = int;
+  static_assert(dealias(^^alias) == ^^int);
+
+  namespace AliasNS = ::std;
+  static_assert(^^::std != ^^AliasNS);
+  static_assert(^^:: == parent_of(^^::std));
+}
+
+int main() {
+  constexpr std::meta::info rint = ^^int; // Ｏ(≧▽≦)Ｏ
   using int2 = [:rint:];
   int2 a = 10;
   assert(a == 10);
@@ -65,5 +117,6 @@ int main () {
   using O_o = [:o_O:]; // 哈哈
 
   test1();
+  test2();
   return 0;
 }
